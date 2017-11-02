9410c27da457c7692f1190a105f831f9a646f86f
frithjofhoppe <frithjof.hoppe@icloud.com>
2017-11-02 09:31:42 +0100

    changes

--- local/CreateBankenstamm.sh
+++ local/CreateBankenstamm.sh
@@ -8,7 +8,7 @@ DROP TABLE SIX_Bankenstamm;
 CREATE TABLE SIX_Bankenstamm ('Sort-Nr.' INTEGER PRIMARY KEY, BCNr INTEGER, 'TEIL. IBAN-Tool' INTEGER,'PZ Validierung' INTEGER, 'BCNR IID' INTEGER, Landcode TEXT, 'Postkonto in' TEXT,'Postkonto out' TEXT, SWIFT TEXT, 'Emailadresse Entwickler' TEXT, Mutationsdatum TEXT, Hauptsitz INTEGER, 'BC-Art' INTEGER, 'Bank / Institut' TEXT, SIC INTEGER, 'euro-sic' INTEGER, Sprache INTEGER, 'Kurz.bez.' TEXT, Domizil TEXT, Postadresse TEXT, PLZ INTEGER, Ort TEXT, Telefon TEXT, Fax TEXT, Vorwahl INTEGER);
 EOF
 
-"" > fileOutput
+rm fileOutput.sql
 
 more SIX_BankenstammCH_3.csv  | while read line
 
@@ -25,17 +25,12 @@ more SIX_BankenstammCH_3.csv  | while read line
 		temp="null"
 	    else
 		isString=false
-		if [[ $temp = *[[:digit:]]* ]];
+		if [[ $temp =~ ^[0-9]+$ ]];
 		then
-		    echo ">> Number <<"
+		    echo "is Number"
 		else
-		    rand="'"
-		    if [ $isString = false ];
-		    then
-			temp="$rand$temp$rand"
-			isString=true
-		    fi
-		    echo " >> String <<"
+		    echo "is String"
+		    temp="'$temp'"
 		fi
 	    fi
 
@@ -47,44 +42,14 @@ more SIX_BankenstammCH_3.csv  | while read line
 	    fi
 	done
 
-	querry="$querry)"
-	echo $querry >> fileOutput
-	sqlite3 $database $querry
-
-#	sortNr=$(echo $line | cut -d";" -f1)
-#	entry[]
-#	bcnr=$(echo $line | cut -d";" -f2)
-#	teilIBANTool=$(echo $line | cut -d";" -f3)
-#	pzValidierung=$(echo $line | cut -d";" -f4)
-#	bcnrIID=$(echo $line | cut -d";" -f5)
-#	landcode=$(echo $line | cut -d";" -f6)
-#	postkontoIn=$(echo $line | cut -d";" -f7)
-#	postkontoOut=$(echo $line | cut -d";" -f8)
-#	swift=$(echo $line | cut -d";" -f9)
-#	mailDev=$(echo $line | cut -d";" -f10)
-#	mutationsdatum=$(echo $line | cut -d";" -f11)
-#	hauptsitz=$(echo $line | cut -d";" -f12)
-#	bcArt=$(echo $line | cut -d";" -f13)
-#	bankInstitut=$(echo $line | cut -d";" -f14)
-#	sic=$(echo $line | cut -d";" -f15)
-#	uroSic=$(echo $line | cut -d";" -f16)
-#	sprache=$(echo $line | cut -d";" -f17)
-#	kurzBez=$(echo $line | cut -d";" -f18)
-#	domizil=$(echo $line | cut -d";" -f19)
-#	postadresse=$(echo $line | cut -d";" -f20)
-#	plz=$(echo $line | cut -d";" -f21)
-#	ort=$(echo $line | cut -d";" -f22)
-#	telefon=$(echo $line | cut -d";" -f23)
-#	fax=$(echo $line | cut -d";" -f24)
-#	vorwahl=$(echo $line | cut -d";" -f25)
-#	sqlite3 $database "INSERT INTO SIX_Bankenstamm VALUES($sortNr,$bcnr,$teilIBANTool,$pzValidierung);"
-	#sqlite3 $database "INSERT INTO SIX_Bankenstamm VALUES($sortNr,$bcnr,$teilIBANTool,$pzValidierung,$brnrIID,'$landcode','$postkontoIn','$postkontoOut','$swift','$mailDev','$mutationsdatum',$hauptsitz,$brArt,'$bankInstitut',$sic,$euroSic,$sprache,'$kurzBez','$domizil','$postadresse',$plz,'$ort','$telefon','$fax',$vorwahl);"
+	querry="$querry);"
+	echo $querry >> fileOutput.sql
     else
 	first=$((first + 1))
     fi
     done
 
-
+sqlite3 $database < fileOutput.sql
 sqlite3 $database "SELECT * FROM SIX_Bankenstamm;"
 
 echo "SUCCESS: imported"
